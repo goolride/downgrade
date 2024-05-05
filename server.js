89811cf8849206d8989fee66d387b839e42bf432
goolride <102709729+goolride@users.noreply.github.com>
2024-05-05 05:25:22 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,18 +8,12 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.post('/send-webhook', async (req, res) => {
-    const { message } = req.body; // Assuming the request body has a 'message' field
-
-    if (!message) {
-        return res.status(400).send('Message field is required');
-    }
-
     try {
-        const webhookUrl = 'https://discord.com/api/webhooks/1233966353581477928/-9BcPdqgj7G-BEdN4oIBVf0cRq85JwwTPD4_F9Xgx2m3cTIsN4USkiE6LYfizmI-F4AJ';
-        await axios.post(webhookUrl, { content: message });
-        res.status(200).send('Webhook sent successfully');
+        const response = await axios.post('https://discord.com/api/webhooks/1233966353581477928/-9BcPdqgj7G-BEdN4oIBVf0cRq85JwwTPD4_F9Xgx2m3cTIsN4USkiE6LYfizmI-F4AJ', req.body);
+        console.log(response.data);
+        res.sendStatus(200);
     } catch (error) {
-        console.error('Error sending webhook:', error);
-        res.status(500).send('Failed to send webhook');
+        console.error(error);
+        res.sendStatus(500);
     }
 });
