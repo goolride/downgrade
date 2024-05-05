d0ddce13a5794242d0bcf389d7a7cd7153794805
goolride <102709729+goolride@users.noreply.github.com>
2024-05-05 03:44:15 -0700

    Update webhook passer.js

--- webhook passer.js
+++ webhook passer.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // POST request handler
-app.post('/discord-webhook', (req, res) => {
+app.post('/https://discord.com/api/webhooks/1233966353581477928/-9BcPdqgj7G-BEdN4oIBVf0cRq85JwwTPD4_F9Xgx2m3cTIsN4USkiE6LYfizmI-F4AJ', (req, res) => {
     // Access the embed data sent in the request body
     const embedData = req.body.embeds[0]; // Assuming only one embed is sent
 
