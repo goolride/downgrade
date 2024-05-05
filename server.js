a45328c7a11971e3a64127fee33772c068241263
goolride <102709729+goolride@users.noreply.github.com>
2024-05-05 06:01:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Listening at ${PORT}`));
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
 
 app.post('/send-webhook' (req,res) => {
-    res.send(req.body)
+    res.send("EHHLo)
 });
