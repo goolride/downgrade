f8a8630f6970670f3fae5bedd214f4100452f70e
goolride <102709729+goolride@users.noreply.github.com>
2024-05-05 04:22:27 -0700

    Update test.cjs

--- test.cjs
+++ test.cjs
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const app = express();
+const cors = require('cors)
+app.use(cors())
 
 app.use(bodyParser.json());
 
