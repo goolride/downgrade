3eb2a8eb578e71376840b28987d76078d0a995e8
goolride <102709729+goolride@users.noreply.github.com>
2024-05-16 16:15:32 -0700

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.listen(PORT, () => console.log(`Listening at ${PORT}`));
-
-app.post('/send-webhook' (req,res) => {
-    res.send("EHHLo)
-});
